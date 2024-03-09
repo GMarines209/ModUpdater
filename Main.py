22106c09750a80425a07807b67e4a1b443d31e21
GMarines209 <gabrielmarines209@gmail.com>
2024-03-08 20:26:51 -0500

    fixing api search

--- Main.py
+++ Main.py
@@ -7,13 +7,15 @@ import ttkbootstrap as tb
 from ttkbootstrap.constants import *
 import pyglet
 from dotenv import load_dotenv
-
 load_dotenv(".env")
 
 pyglet.font.add_file("Minecrafter.Reg.ttf")
 Minecrafter =  pyglet.font.load("Minecrafter")
 
-
+versions = []
+mods=[]
+n_mods = []
+loader = []
 Api_key: str = os.getenv("API_KEY")
 
 headers = {
@@ -21,8 +23,6 @@ headers = {
   'x-api-key': Api_key
 }
 r = requests.get('https://api.curseforge.com/v1/minecraft/version', headers = headers)
-
-versions = []
 
 if r.status_code == 200:
   # Process the response data
@@ -32,19 +32,30 @@ if r.status_code == 200:
   for game_entry in data["data"]:
     game_version = game_entry["versionString"]
     versions.append(game_version)
-
 else:
   # Handle unsuccessful response
   print(f"Error retrieving data: {r.status_code}")
+ 
 
+def mod_search(e):
+    r = requests.get('https://api.curseforge.com/v1/mods/search', params={
+    'gameId': '432',
+    'gameVersion': vsCombo.get(),
+    'searchFilter': n_mods
+    }, headers = headers)
+
+    print(r.json())
+
+'''
+def mod_download():
+   r = requests.get('https://api.curseforge.com/v1/mods/{modId}', headers = headers)
+'''
 
 
 
 #open file  
 def ModFiles():
-    
-    #creating empty mods list
-    mods=[]
+        
     username = os.getlogin()
 
     filenames = filedialog.askopenfilenames( #if user doesnt have a mods folder it just opens on default directory (documents)
@@ -56,11 +67,27 @@ def ModFiles():
     for filename in filenames:
         mod_name = ( os.path.basename(filename))
         mods.append(mod_name)
-
+        for mod_name in mods:
+            parts = mod_name.split("-", 1)
+            if(len(parts) > 0 ):
+               new_name = (parts[0])
+               n_mods.append(new_name)
+            else:
+               None
+    
+    
+    if "fabric" in mod_name:
+       print("fabric")
+    elif "forge" in mod_name:
+       print("forge")
+    
     path  = os.path.dirname(filename)
     path_text = (path + filename)
-    path_entry.insert(0,path_text)
-        
+    path_entry.insert(0,path_text)   
+
+    print(n_mods)
+
+
 # get download location of new mods
 def DownloadPath():
     filepath = filedialog.askdirectory(
@@ -110,10 +137,12 @@ path_entry.grid(padx= 100,pady= 60,sticky="e",row = 2, column= 2,)
 vsCombo = tb.Combobox( 
     root,
     bootstyle ="darkly",
-    values= (versions)
+    values= (versions),
       )   
 vsCombo.grid(row = 4, column= 2)
+vsCombo.bind("<<ComboboxSelected>>", mod_search)
 vsCombo.current(0)
+
 
 # second button for download path selection
 path_button = tb.Button(   
@@ -130,8 +159,6 @@ sub_button = tb.Button(
 )
 sub_button.grid(row = 6, column= 2)
 sub_button.configure(state="disabled") # Disabled on default until user selects all 3 catagories
-
-
 
 
 
